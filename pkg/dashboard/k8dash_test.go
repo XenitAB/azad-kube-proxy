0fb713f126b207eb46fd35f61e32cd86e3d5f5fe
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-02-17 22:40:35 +0100

    Add additional test

--- pkg/dashboard/k8dash_test.go
+++ pkg/dashboard/k8dash_test.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
-	"os"
+	"net/http/httputil"
+	"net/url"
 	"strings"
 	"testing"
 
@@ -28,10 +29,6 @@ func TestNewK8sdashClient(t *testing.T) {
 
 func TestK8dashDashboardHandler(t *testing.T) {
 	tenantID := getEnvOrSkip(t, "TENANT_ID")
-
-	restoreK8dashPath := tempChangeEnv("K8DASH_PATH", "../../tmp/k8dash/build/")
-	defer restoreK8dashPath()
-
 	ctx := logr.NewContext(context.Background(), logr.DiscardLogger{})
 
 	req, err := http.NewRequest("GET", "/", nil)
@@ -69,8 +66,69 @@ func TestK8dashDashboardHandler(t *testing.T) {
 	}
 }
 
-func tempChangeEnv(key, value string) func() {
-	oldEnv := os.Getenv(key)
-	os.Setenv(key, value)
-	return func() { os.Setenv(key, oldEnv) }
+func TestK8dashPreAuth(t *testing.T) {
+	tenantID := getEnvOrSkip(t, "TENANT_ID")
+	ctx := logr.NewContext(context.Background(), logr.DiscardLogger{})
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Errorf("Expected err to be nil but it was %q", err)
+	}
+
+	req.Header.Add("Authorization", "Bearer fake-token")
+
+	config := config.Config{
+		TenantID: tenantID,
+	}
+
+	client, err := newK8dashClient(ctx, config)
+	if err != nil {
+		t.Errorf("Expected err to be nil but it was %q", err)
+	}
+
+	fakeBackend := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("{\"fake\": true}"))
+	}))
+	defer fakeBackend.Close()
+	fakeBackendURL, err := url.Parse(fakeBackend.URL)
+	if err != nil {
+		t.Errorf("Expected err to be nil but it was %q", err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(fakeBackendURL)
+
+	rr := httptest.NewRecorder()
+	router := mux.NewRouter()
+	router.Handle("/", proxy)
+	router.Use(client.preAuth)
+	router.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if rr.Code != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expectedStringContains := "{\"fake\": true}"
+	if !strings.Contains(rr.Body.String(), expectedStringContains) {
+		t.Errorf("handler returned unexpected body: got %v expected to contain %v",
+			rr.Body.String(), expectedStringContains)
+	}
+
+	expectedCookieFound := false
+	authorizationCookie := &http.Cookie{}
+	for _, cookie := range rr.Result().Cookies() {
+		if cookie.Name == "Authorization" {
+			expectedCookieFound = true
+			authorizationCookie = cookie
+		}
+	}
+
+	if !expectedCookieFound {
+		t.Error("expected cookie 'Authorization' not found in response")
+	}
+
+	if expectedCookieFound && authorizationCookie.Value != "fake-token" {
+		t.Errorf("cookie 'Authorization' did not contain expected 'fake-token' but: %s", authorizationCookie.Value)
+	}
 }
