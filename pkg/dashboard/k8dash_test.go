8eb91f754ba16df7b579feda21033700adc0695d
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-02-18 10:45:21 +0100

    Add more tests

--- pkg/dashboard/k8dash_test.go
+++ pkg/dashboard/k8dash_test.go
@@ -2,6 +2,7 @@ package dashboard
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"net/http/httputil"
@@ -70,13 +71,6 @@ func TestK8dashPreAuth(t *testing.T) {
 	tenantID := getEnvOrSkip(t, "TENANT_ID")
 	ctx := logr.NewContext(context.Background(), logr.DiscardLogger{})
 
-	req, err := http.NewRequest("GET", "/", nil)
-	if err != nil {
-		t.Errorf("Expected err to be nil but it was %q", err)
-	}
-
-	req.Header.Add("Authorization", "Bearer fake-token")
-
 	config := config.Config{
 		TenantID: tenantID,
 	}
@@ -95,40 +89,293 @@ func TestK8dashPreAuth(t *testing.T) {
 		t.Errorf("Expected err to be nil but it was %q", err)
 	}
 
-	proxy := httputil.NewSingleHostReverseProxy(fakeBackendURL)
-
-	rr := httptest.NewRecorder()
-	router := mux.NewRouter()
-	router.Handle("/", proxy)
-	router.Use(client.preAuth)
-	router.ServeHTTP(rr, req)
-
-	// Check the status code is what we expect.
-	if rr.Code != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, http.StatusOK)
+	cases := []struct {
+		requestFunc            func() (*http.Request, string, error)
+		expectedStringContains string
+		expectedStatusCode     int
+	}{
+		{
+			requestFunc: func() (*http.Request, string, error) {
+				req, err := http.NewRequest("GET", "/", nil)
+				if err != nil {
+					return nil, "", fmt.Errorf("Expected err to be nil but it was %q", err)
+				}
+				req.Header.Add("Authorization", "Bearer fake-token")
+				return req, "fake-token", nil
+			},
+			expectedStringContains: "{\"fake\": true}",
+			expectedStatusCode:     http.StatusOK,
+		},
+		{
+			requestFunc: func() (*http.Request, string, error) {
+				req, err := http.NewRequest("GET", "/", nil)
+				if err != nil {
+					return nil, "", fmt.Errorf("Expected err to be nil but it was %q", err)
+				}
+				req.Header.Add("Authorization", "Bearer")
+				return req, "", nil
+			},
+			expectedStringContains: "{\"fake\": true}",
+			expectedStatusCode:     http.StatusOK,
+		},
+		{
+			requestFunc: func() (*http.Request, string, error) {
+				req, err := http.NewRequest("GET", "/", nil)
+				if err != nil {
+					return nil, "", fmt.Errorf("Expected err to be nil but it was %q", err)
+				}
+				req.Header.Add("Authorization", "Bearer ")
+				return req, "", nil
+			},
+			expectedStringContains: "{\"fake\": true}",
+			expectedStatusCode:     http.StatusOK,
+		},
+		{
+			requestFunc: func() (*http.Request, string, error) {
+				req, err := http.NewRequest("GET", "/", nil)
+				if err != nil {
+					return nil, "", fmt.Errorf("Expected err to be nil but it was %q", err)
+				}
+				return req, "", nil
+			},
+			expectedStringContains: "{\"fake\": true}",
+			expectedStatusCode:     http.StatusOK,
+		},
+		{
+			requestFunc: func() (*http.Request, string, error) {
+				req, err := http.NewRequest("GET", "/", nil)
+				if err != nil {
+					return nil, "", fmt.Errorf("Expected err to be nil but it was %q", err)
+				}
+				req.Header.Add("Authorization", "abc 123 fake")
+				return req, "", nil
+			},
+			expectedStringContains: "{\"fake\": true}",
+			expectedStatusCode:     http.StatusOK,
+		},
+		{
+			requestFunc: func() (*http.Request, string, error) {
+				req, err := http.NewRequest("GET", "/", nil)
+				if err != nil {
+					return nil, "", fmt.Errorf("Expected err to be nil but it was %q", err)
+				}
+				req.Header.Add("Authorization", "Bearer token-fake")
+				return req, "token-fake", nil
+			},
+			expectedStringContains: "{\"fake\": true}",
+			expectedStatusCode:     http.StatusOK,
+		},
 	}
 
-	// Check the response body is what we expect.
-	expectedStringContains := "{\"fake\": true}"
-	if !strings.Contains(rr.Body.String(), expectedStringContains) {
-		t.Errorf("handler returned unexpected body: got %v expected to contain %v",
-			rr.Body.String(), expectedStringContains)
-	}
+	for _, c := range cases {
+		req, expectedCookieValue, err := c.requestFunc()
+		if err != nil {
+			t.Errorf("Expected err to be nil but it was %q", err)
+		}
 
-	expectedCookieFound := false
-	authorizationCookie := &http.Cookie{}
-	for _, cookie := range rr.Result().Cookies() {
-		if cookie.Name == "Authorization" {
-			expectedCookieFound = true
-			authorizationCookie = cookie
+		proxy := httputil.NewSingleHostReverseProxy(fakeBackendURL)
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+		router.Handle("/", proxy)
+		router.Use(client.preAuth)
+		router.ServeHTTP(rr, req)
+
+		// Check the status code is what we expect.
+		if rr.Code != c.expectedStatusCode {
+			t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, c.expectedStatusCode)
+		}
+
+		if !strings.Contains(rr.Body.String(), c.expectedStringContains) {
+			t.Errorf("handler returned unexpected body: got %v expected to contain %v",
+				rr.Body.String(), c.expectedStringContains)
+		}
+
+		if expectedCookieValue != "" {
+			expectedCookieFound := false
+			authorizationCookie := &http.Cookie{}
+			for _, cookie := range rr.Result().Cookies() {
+				if cookie.Name == "Authorization" {
+					expectedCookieFound = true
+					authorizationCookie = cookie
+				}
+			}
+
+			if !expectedCookieFound {
+				t.Error("expected cookie 'Authorization' not found in response")
+			}
+
+			if expectedCookieFound && authorizationCookie.Value != expectedCookieValue {
+				t.Errorf("cookie 'Authorization' did not contain expected '%s' but: %s", expectedCookieValue, authorizationCookie.Value)
+			}
+		}
+
+		if expectedCookieValue == "" {
+			expectedCookieFound := false
+			for _, cookie := range rr.Result().Cookies() {
+				if cookie.Name == "Authorization" {
+					expectedCookieFound = true
+				}
+			}
+
+			if expectedCookieFound {
+				t.Error("expected cookie 'Authorization' was not expected to be found in response")
+			}
 		}
 	}
+}
 
-	if !expectedCookieFound {
-		t.Error("expected cookie 'Authorization' not found in response")
+func TestK8dashGetOIDC(t *testing.T) {
+	tenantID := getEnvOrSkip(t, "TENANT_ID")
+	ctx := logr.NewContext(context.Background(), logr.DiscardLogger{})
+
+	cases := []struct {
+		requestFunc            func() (*http.Request, error)
+		config                 config.Config
+		expectedStringContains string
+		expectedStatusCode     int
+	}{
+		{
+			requestFunc: func() (*http.Request, error) {
+				req, err := http.NewRequest("GET", "/oidc", nil)
+				if err != nil {
+					return nil, fmt.Errorf("Expected err to be nil but it was %q", err)
+				}
+				return req, nil
+			},
+			config: config.Config{
+				TenantID: tenantID,
+				K8dashConfig: config.K8dashConfig{
+					ClientID: "00000000-0000-0000-0000-000000000000",
+					Scope:    "fake-scope",
+				},
+			},
+			expectedStringContains: fmt.Sprintf("https://login.microsoftonline.com/%s/oauth2/v2.0/authorize", tenantID),
+			expectedStatusCode:     http.StatusOK,
+		},
+		{
+			requestFunc: func() (*http.Request, error) {
+				req, err := http.NewRequest("GET", "/oidc", nil)
+				if err != nil {
+					return nil, fmt.Errorf("Expected err to be nil but it was %q", err)
+				}
+				return req, nil
+			},
+			config: config.Config{
+				TenantID: tenantID,
+				K8dashConfig: config.K8dashConfig{
+					ClientID: "00000000-0000-0000-0000-000000000000",
+					Scope:    "fake-scope",
+				},
+			},
+			expectedStringContains: "client_id=00000000-0000-0000-0000-000000000000",
+			expectedStatusCode:     http.StatusOK,
+		},
+		{
+			requestFunc: func() (*http.Request, error) {
+				req, err := http.NewRequest("GET", "/oidc", nil)
+				if err != nil {
+					return nil, fmt.Errorf("Expected err to be nil but it was %q", err)
+				}
+				return req, nil
+			},
+			config: config.Config{
+				TenantID: tenantID,
+				K8dashConfig: config.K8dashConfig{
+					ClientID: "00000000-0000-0000-0000-000000000000",
+					Scope:    "fake-scope",
+				},
+			},
+			expectedStringContains: "scope=fake-scope",
+			expectedStatusCode:     http.StatusOK,
+		},
 	}
 
-	if expectedCookieFound && authorizationCookie.Value != "fake-token" {
-		t.Errorf("cookie 'Authorization' did not contain expected 'fake-token' but: %s", authorizationCookie.Value)
+	for _, c := range cases {
+		client, err := newK8dashClient(ctx, c.config)
+		if err != nil {
+			t.Errorf("Expected err to be nil but it was %q", err)
+		}
+
+		req, err := c.requestFunc()
+		if err != nil {
+			t.Errorf("Expected err to be nil but it was %q", err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+		router.HandleFunc("/oidc", client.getOIDC(ctx)).Methods("GET")
+		router.ServeHTTP(rr, req)
+
+		// Check the status code is what we expect.
+		if rr.Code != c.expectedStatusCode {
+			t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, c.expectedStatusCode)
+		}
+
+		if !strings.Contains(rr.Body.String(), c.expectedStringContains) {
+			t.Errorf("handler returned unexpected body: got %v expected to contain %v",
+				rr.Body.String(), c.expectedStringContains)
+		}
+	}
+}
+
+func TestK8dashPostOIDC(t *testing.T) {
+	tenantID := getEnvOrSkip(t, "TENANT_ID")
+	ctx := logr.NewContext(context.Background(), logr.DiscardLogger{})
+
+	cases := []struct {
+		requestFunc            func() (*http.Request, error)
+		config                 config.Config
+		expectedStringContains string
+		expectedStatusCode     int
+	}{
+		{
+			requestFunc: func() (*http.Request, error) {
+				req, err := http.NewRequest("POST", "/oidc", nil)
+				if err != nil {
+					return nil, fmt.Errorf("Expected err to be nil but it was %q", err)
+				}
+				return req, nil
+			},
+			config: config.Config{
+				TenantID: tenantID,
+				K8dashConfig: config.K8dashConfig{
+					ClientID:     "00000000-0000-0000-0000-000000000000",
+					ClientSecret: "fake-secret",
+					Scope:        "fake-scope",
+				},
+			},
+			expectedStringContains: "",
+			expectedStatusCode:     http.StatusInternalServerError,
+		},
+	}
+
+	for _, c := range cases {
+		client, err := newK8dashClient(ctx, c.config)
+		if err != nil {
+			t.Errorf("Expected err to be nil but it was %q", err)
+		}
+
+		req, err := c.requestFunc()
+		if err != nil {
+			t.Errorf("Expected err to be nil but it was %q", err)
+		}
+
+		rr := httptest.NewRecorder()
+		router := mux.NewRouter()
+		router.HandleFunc("/oidc", client.postOIDC(ctx)).Methods("POST")
+		router.ServeHTTP(rr, req)
+
+		// Check the status code is what we expect.
+		if rr.Code != c.expectedStatusCode {
+			t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, c.expectedStatusCode)
+		}
+
+		if c.expectedStringContains != "" {
+			if !strings.Contains(rr.Body.String(), c.expectedStringContains) {
+				t.Errorf("handler returned unexpected body: got %v expected to contain %v",
+					rr.Body.String(), c.expectedStringContains)
+			}
+		}
 	}
 }
