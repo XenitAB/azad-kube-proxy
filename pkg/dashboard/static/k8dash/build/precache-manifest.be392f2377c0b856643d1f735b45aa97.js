537b63ca0dd37eaa63cdfc5e82a33c52d2fea0a4
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-02-23 09:28:43 +0100

    bump static file

---
+++ pkg/dashboard/static/k8dash/build/precache-manifest.be392f2377c0b856643d1f735b45aa97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d724abbbdbbeb0060b0322638ec1f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "68a384b87b4a1986d54e",
+    "url": "./static/css/2.b522e268.chunk.css"
+  },
+  {
+    "revision": "197bc9ecf3290db87231",
+    "url": "./static/css/main.49b3e53b.chunk.css"
+  },
+  {
+    "revision": "68a384b87b4a1986d54e",
+    "url": "./static/js/2.cfa836c5.chunk.js"
+  },
+  {
+    "revision": "90b280b61b0c9cea9303e3d304e1c4cc",
+    "url": "./static/js/2.cfa836c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "197bc9ecf3290db87231",
+    "url": "./static/js/main.42e79d25.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
