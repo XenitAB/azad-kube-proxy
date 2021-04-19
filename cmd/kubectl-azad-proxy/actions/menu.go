7ad0c826e50fa78e88007fff8ac22605b0ac0fef
Simon Gottschlag <simon.gottschlag@xenit.se>
2021-04-19 11:45:38 +0200

    Add comments

--- cmd/kubectl-azad-proxy/actions/menu.go
+++ cmd/kubectl-azad-proxy/actions/menu.go
@@ -45,6 +45,7 @@ func MenuFlags(ctx context.Context) []cli.Flag {
 func Menu(ctx context.Context, cfg MenuConfig) error {
 	log := logr.FromContext(ctx)
 
+	// Run discovery of Azure AD applications
 	apps, err := runDiscover(ctx, *cfg.discoverConfig)
 	if err != nil {
 		log.V(1).Info("Unable to run discovery", "error", err.Error())
@@ -70,12 +71,12 @@ func Menu(ctx context.Context, cfg MenuConfig) error {
 	}
 
 	idx, _, err := clusterPrompt.Run()
-
 	if err != nil {
 		log.V(1).Info("Unable to menu prompt", "error", err.Error())
 		return customerrors.New(customerrors.ErrorTypeMenu, err)
 	}
 
+	// apps[idx] is the selected cluster
 	cluster := apps[idx]
 	proxyURL, err := url.Parse(cluster.ProxyURL)
 	if err != nil {
@@ -83,6 +84,7 @@ func Menu(ctx context.Context, cfg MenuConfig) error {
 		return customerrors.New(customerrors.ErrorTypeMenu, err)
 	}
 
+	// Update the generateConfig based on the selected cluster (overwrite = false)
 	cfg.generateConfig.Merge(GenerateConfig{
 		clusterName: cluster.ClusterName,
 		resource:    cluster.Resource,
@@ -92,6 +94,7 @@ func Menu(ctx context.Context, cfg MenuConfig) error {
 
 	err = Generate(ctx, *cfg.generateConfig)
 
+	// If the config already exists inside of KubeConfig, ask user if it should be overwritten
 	if customerrors.To(err).ErrorType == customerrors.ErrorTypeOverwriteConfig {
 		overwritePrompt := promptui.Select{
 			Label: "Do you want to overwrite the config",
@@ -104,16 +107,15 @@ func Menu(ctx context.Context, cfg MenuConfig) error {
 			return customerrors.New(customerrors.ErrorTypeMenu, err)
 		}
 
+		// If user chose 'No' to overwrite, exit
 		if result == "No" {
 			log.V(0).Info("User selected not to overwrite config")
 			return nil
 		}
 
+		// If user chose 'Yes', update config to allow overwrite and run again
 		cfg.generateConfig.Merge(GenerateConfig{
-			clusterName: cluster.ClusterName,
-			resource:    cluster.Resource,
-			proxyURL:    *proxyURL,
-			overwrite:   true,
+			overwrite: true,
 		})
 
 		err = Generate(ctx, *cfg.generateConfig)
@@ -133,6 +135,7 @@ func Menu(ctx context.Context, cfg MenuConfig) error {
 	return nil
 }
 
+// unrequireFlags takes a []cli.Flag array 'f' and sets Required to false in all flags
 func unrequireFlags(f []cli.Flag) []cli.Flag {
 	flags := f
 	for _, flag := range flags {
@@ -149,6 +152,7 @@ func unrequireFlags(f []cli.Flag) []cli.Flag {
 	return flags
 }
 
+// mergeFlags taks two arrays ('a' and 'b') and removes any duplicates (based on the name) and outputs a merged array
 func mergeFlags(a []cli.Flag, b []cli.Flag) []cli.Flag {
 	flags := a
 
@@ -161,6 +165,7 @@ func mergeFlags(a []cli.Flag, b []cli.Flag) []cli.Flag {
 	return flags
 }
 
+// duplicateFlag identified is the flag 'b' (based on the name) exists in the array 'a'
 func duplicateFlag(a []cli.Flag, b cli.Flag) bool {
 	duplicate := false
 
