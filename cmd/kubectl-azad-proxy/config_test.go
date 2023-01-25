2c6b2d2066ecf22369e049d64c749f7432820e24
Simon Gottschlag <simon.gottschlag@xenit.se>
2023-01-25 12:34:28 +0100

    Fix kubeconfig default path

--- cmd/kubectl-azad-proxy/config_test.go
+++ cmd/kubectl-azad-proxy/config_test.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestNewConfig(t *testing.T) {
+	envVarsToClear := []string{
+		"AZURE_CLIENT_ID",
+		"AZURE_CLIENT_SECRET",
+		"AZURE_TENANT_ID",
+		"CLUSTER_NAME",
+		"EXCLUDE_AZURE_CLI_AUTH",
+		"EXCLUDE_ENVIRONMENT_AUTH",
+		"EXCLUDE_MSI_AUTH",
+		"KUBECONFIG",
+		"OUTPUT",
+		"OVERWRITE_KUBECONFIG",
+		"PROXY_URL",
+		"RESOURCE",
+		"TLS_INSECURE_SKIP_VERIFY",
+		"TOKEN_CACHE_DIR",
+	}
+
+	for _, envVar := range envVarsToClear {
+		restore := testTempUnsetEnv(t, envVar)
+		defer restore()
+	}
+	userHomeDir, err := os.UserHomeDir()
+	require.NoError(t, err)
+	defaultKubeConfig := filepath.Clean(fmt.Sprintf("%s/.kube/config", userHomeDir))
+
 	t.Run("no subcommand", func(t *testing.T) {
 		args := []string{
 			"/foo/bar/bin",
@@ -90,6 +118,7 @@ func TestNewConfig(t *testing.T) {
 			ClusterName: "ze-cluster",
 			ProxyURL:    "ze-proxy-url",
 			Resource:    "ze-resource",
+			KubeConfig:  defaultKubeConfig,
 		}
 		require.Equal(t, expectedGenerateConfig, *cfg.Generate)
 	})
@@ -125,6 +154,7 @@ func TestNewConfig(t *testing.T) {
 		expectedLoginConfig := loginConfig{
 			ClusterName: "ze-cluster",
 			Resource:    "ze-resource",
+			KubeConfig:  defaultKubeConfig,
 		}
 		require.Equal(t, expectedLoginConfig, *cfg.Login)
 	})
@@ -138,8 +168,17 @@ func TestNewConfig(t *testing.T) {
 		require.NoError(t, err)
 		require.NotNil(t, cfg.Menu)
 		expectedMenuConfig := menuConfig{
-			Output: "TABLE",
+			Output:     "TABLE",
+			KubeConfig: defaultKubeConfig,
 		}
 		require.Equal(t, expectedMenuConfig, *cfg.Menu)
 	})
+}
+
+func testTempUnsetEnv(t *testing.T, key string) func() {
+	t.Helper()
+
+	oldEnv := os.Getenv(key)
+	os.Unsetenv(key)
+	return func() { os.Setenv(key, oldEnv) }
 }
